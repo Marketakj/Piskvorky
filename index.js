968eba65d47666055b6c9b08e6fac222744ba793
Marketa KJ <mkloubkovajancikova@gmail.com>
2021-04-23 23:30:03 +0200

    Zaklad js

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log('funguje');
+
+/*const circlePlays = ;
+
+/*let activePlayer = 'circle',*/
+
+const button = document.querySelectorAll('button-hra');
+
+button.addEventListener = (event) => {
+  'click';
+  button.src = 'circle.svg';
+};
+
+/*const goodbye = (name) => {
+  return `S pozdravem ${name}`;
+};*/
+
+/*if (activePlayer === circle;)*/
