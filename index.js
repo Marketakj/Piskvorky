85c9b313f76bf4aa040cc9a0de5465f32fb40384
Marketa KJ <mkloubkovajancikova@gmail.com>
2021-04-24 22:28:09 +0200

    Pridany atribut hraje:

--- index.js
+++ index.js
@@ -2,23 +2,20 @@
 
 console.log('funguje');
 
-/*const circle = document.querySelectorAll('.button-hra');
-
-circle.addEventListener('click', () => {
-  circle.classList.add('boardfield');
-});*/
-
 const cross = document.querySelector('.button-hra');
 const circle = document.querySelector('.button-hra');
 
 let naTahu = circle; 
 
-if (jeNaTahu === 'circle') {
-  circle.addEventListener('click', () => {
-  circle.classList.add('boardfield');
-  jeNaTahu === 'cross'
-  } else {
-  cross.addEventListener('click', () => {
-    cross.classList.add('boardfield');
-    jeNaTahu === 'circle';
-  });
+if (naTahu === 'circle') {
+    circle.addEventListener('click', () => {
+    circle.classList.add('boardfield__circle'));
+    naTahu === 'cross';
+    
+
+    } else {
+    cross.addEventListener('click', () => {
+    cross.classList.add('boardfield__cross'));
+    naTahu === 'circle');
+    document.querySelector('.hraje').src = 'circle.svg';
+    )};
