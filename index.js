c1a195dad005cd86eb1ca1d350f8698e48dc05e8
Marketa KJ <mkloubkovajancikova@gmail.com>
2021-05-08 17:43:37 +0200

    oprava kodu dle code review

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 'use strict';
 
-console.log('funguje');
-
-const cross = document.querySelector('.button-hra');
+/*const cross = document.querySelector('.button-hra');
 const circle = document.querySelector('.button-hra');
 
-let naTahu = circle; 
+let naTahu = cross;
 
-if (naTahu === 'circle') {
-    circle.addEventListener('click', () => {
-    circle.classList.add('boardfield__circle')};
-    naTahu === 'cross';
-    } 
-    else 
-    {
-    cross.addEventListener('click', () => {
-    cross.classList.add('boardfield__cross')};
+if (naTahu === 'cross') {
+  circle.addEventListener('click', () => {
+    circle.classList.add('boardfield__cross');
     naTahu === 'circle';
-    document.querySelector('.hraje').src = 'circle.svg';
-    };
+  });
+} else {
+  cross.addEventListener('click', () => {
+    cross.classList.add('boardfield__circle');
+    naTahu === 'cross';
+    /*document.querySelector('.hraje').src = 'circle.svg';
+  });
+}*/
+
+let naTahu = 'circle';
+document.querySelectorAll('.button-hra').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (naTahu === 'circle') {
+      btn.classList.add('boardfield__circle');
+      document.querySelector('.circle').src = 'circle.svg';
+      naTahu = 'cross';
+    } else {
+      btn.classList.add('boardfield__cross');
+      document.querySelector('.circle').src = 'cross.svg';
+      naTahu = 'circle';
+    }
+  });
+});
